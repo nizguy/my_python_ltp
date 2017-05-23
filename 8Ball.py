39592ea7a2967b5826e355c41d853d5a7223ecd6
Mark Burns <mark.007.99@gmail.com>
2017-05-23 14:02:51 -0600

    Add files via upload

---
+++ 8Ball.py
@@ -0,0 +1,45 @@
+import random
+
+question = raw_input("What is your question my friend? ")
+answer = random.randint(1,20)
+
+if answer == 1:
+    print "Signs Point To Yes"
+elif answer == 2:
+    print "Yes"
+elif answer == 3:
+    print "Reply hazy, try again."
+elif answer == 4:
+    print "Without a doubt."
+elif answer == 5:
+    print "All sources say no."
+elif answer == 6:
+    print "As I see it, yes."
+elif answer == 7:
+    print "You may rely on it."
+elif answer == 8:
+    print "Concentrate and ask again."
+elif answer == 9:
+    print "Outlook not so good."
+elif answer == 10:
+    print "It is decidedly so."
+elif answer == 11:
+    print "Yes"
+elif answer == 12:
+    print "Reply hazy, try again."
+elif answer == 13:
+    print "Without a doubt."
+elif answer == 14:
+    print "All sources say no."
+elif answer == 15:
+    print "As I see it, yes."
+elif answer == 16:
+    print "You may rely on it."
+elif answer == 17:
+    print "Concentrate and ask again."
+elif answer == 18:
+    print "Outlook not so good."
+elif answer == 19:
+    print "It is decidedly so."
+elif answer == 20:
+    print "It is decidedly so."
