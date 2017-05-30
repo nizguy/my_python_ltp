e2aa3732c116685b35dc1eda889c883569a15098
Mark Burns <mark.007.99@gmail.com>
2017-05-30 08:22:40 -0600

    Update lab1-id.py

--- lab1-id.py
+++ lab1-id.py
@@ -6,7 +6,7 @@ print "My name is {}." .format(name)
 print "My age is {} years old." .format(age)
 print "My average GPA was {}." .format(gpa)
 
-operand1 = 978.425?
+operand1 = 978.425
 operand2 = 0.0324
 
 print "{} + {} = {}" .format(operand1, operand2, operand1 + operand2)
