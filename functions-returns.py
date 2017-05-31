fc346d6163dfd1e188a0e2512cb301f6fc600c6c
Mark Burns <mark.007.99@gmail.com>
2017-05-31 08:31:27 -0600

    Add files via upload

---
+++ functions-returns.py
@@ -0,0 +1,7 @@
+def areaOfRectangle(length,width):
+    area = length * width
+    return area
+area = areaOfRectangle(10,12)
+areab = areaOfRectangle(100,120)
+print "The area of the rectangle is", area
+print "The area of rectangle B is", areab
