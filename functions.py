9d32ec652cda21c1926459399c5f4469dce6aac6
Mark Burns <mark.007.99@gmail.com>
2017-05-31 08:14:47 -0600

    Added File functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+def greetTheUser():
+    "This function greets the user"
+    print "Hello!"
+    print "Welcome to my nightmare."
+
+greetTheUser()
+greetTheUser()
+print "This is more stuff being printed outside the function function in python."
+greetTheUser()
