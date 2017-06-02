85c4786500136d6edecb0da6e0348d3be7dec9b7
Mark Burns <mark.007.99@gmail.com>
2017-06-02 12:04:25 -0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,29 @@
+# names = ["Mark" , "Fred" , "Tom" , "Wendy" , "Kevin" ]
+# gpas = [ 3.41, 2.55, 4.0, 2.95, 3.75 ]
+
+# print names[0]
+# print names[3]
+
+# print gpas[0]
+# names.append("Brett")
+# print names[5]
+# del names[5]
+
+# for name in names:
+#   print name
+
+# gpas.append(1.55)
+
+# total = 0
+# for score in gpas:
+#     total = total + score
+# print "The average GPA is ", (total/len(gpas))
+
+######
+
+months = ("Jan." , "Feb." , "Mar." , "Apr." , "May" , "Jun." , "Jul." , "Aug." , "Sept." , "Oct." , "Nov." , "Dec.")
+
+print months[0]
+
+for month in months:
+    print month
